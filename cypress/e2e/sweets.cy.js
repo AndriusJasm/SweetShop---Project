f2ad5ea1f3d17691ae3b10dac473771a9b36edaf
AndriusJasm <jasmontai@gmail.com>
2025-04-02 20:09:56 +0300

    update

--- cypress/e2e/sweets.cy.js
+++ cypress/e2e/sweets.cy.js
@@ -1,8 +1,12 @@
 describe("SweetShop - Sweets Page", () => {
-    const url = "https://sweetshop.netlify.app/sweets";
+    const productList = ".col-lg-3";
+    const productName = ".card-title";
+    const productPrice = ".text-muted";
+    const productImage = ".card-img-top";
+    const addToBasketBtn = ".btn";
 
     beforeEach(() => {
-        cy.visit(url);
+        cy.visitSweetsPage();
     });
 
     it('TC_3.1 Verify the page title is "Browse sweets"', () => {
@@ -10,23 +14,23 @@ describe("SweetShop - Sweets Page", () => {
     });
 
     it("TC_3.2 Verify the list of available products is displayed", () => {
-        cy.get(".container").should("exist").and("be.visible");
+        cy.get(productList).should("exist").and("be.visible");
     });
 
     it('TC_3.3 Verify each product has a name, price, image, and "Add to Basket" button', () => {
-        cy.get(".col-lg-3").each(($el) => {
-            cy.wrap($el).find(".card-title").should("be.visible");
-            cy.wrap($el).find(".text-muted").should("be.visible");
+        cy.get(productList).each(($el) => {
+            cy.wrap($el).find(productName).should("be.visible");
+            cy.wrap($el).find(productPrice).should("be.visible");
+            cy.wrap($el).find(addToBasketBtn).should("be.visible");
             // Verify the image is actually displayed
             cy.wrap($el)
-                .find(".card-img-top")
+                .find(productImage)
                 .should("be.visible")
                 .should(($img) => {
                     expect($img[0].complete).to.be.true; // Ensures image loading is finished
                     expect($img[0].naturalWidth).to.be.greaterThan(0); // Image must be loaded properly
                     expect($img[0].clientWidth).to.be.greaterThan(0); // Ensures it's displayed
                 });
-            cy.wrap($el).find(".btn").should("be.visible");
         });
     });
 });
